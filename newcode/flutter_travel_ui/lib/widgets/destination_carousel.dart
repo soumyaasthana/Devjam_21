2bfea7c5ac7c9c01216e860930dc9f23f2bc944f
vanshika462 <35446219+vanshika462@users.noreply.github.com>
2021-05-19 21:44:28 +0400

    Update destination_carousel.dart

--- newcode/flutter_travel_ui/lib/widgets/destination_carousel.dart
+++ newcode/flutter_travel_ui/lib/widgets/destination_carousel.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_travel_ui/models/destination_model.dart';
-import 'package:flutter_travel_ui/screens/destination_screen.dart';
+import 'package:yatravel/models/destination_model.dart';
+import 'package:yatravel/screens/destination_screen.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class DestinationCarousel extends StatelessWidget {
