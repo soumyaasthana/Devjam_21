c23d16b6b1de60f9c0001075d76a8e02ecf40655
vanshika462 <35446219+vanshika462@users.noreply.github.com>
2021-05-19 21:45:25 +0400

    Update main.dart

--- newcode/flutter_travel_ui/lib/main.dart
+++ newcode/flutter_travel_ui/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_travel_ui/screens/home_screen.dart';
+import 'package:yatravel/screens/home_screen.dart';
 
 void main() => runApp(MyApp());
 
